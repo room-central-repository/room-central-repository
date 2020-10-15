a91264819550040baf3ee8dcff4a034a71dcad19
Esteban Calcagno <estebancalca@gmail.com>
2020-10-15 10:44:01 -0300

    Update Install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
-sudo apt install -y git
-git clone https://github.com/room-central-repository/room-central-repository
+#!/bin/sh
 sudo apt-get install -y qt5-default
 sudo apt-get install -y g++
 sudo apt-get install -y make
-cd room-central-repository
 qmake "CONFIG+=nosound headless" Sagora.pro
 make clean
 make
